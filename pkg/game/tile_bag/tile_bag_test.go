1b0a43a78e1a9f734f537b9d3d4227345be1a4d4
Daniel Vaughan <dan@danielvaughan.com>
2018-10-04 16:36:03 +0200

    added tile bag

---
+++ pkg/game/tile_bag/tile_bag_test.go
@@ -0,0 +1,27 @@
+package tile_bag
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestPickingTiles(t *testing.T) {
+	tiles := []Tile{{'A', 1}, {'B', 1}, {'C', 1}}
+	tg := NewTileBag(tiles)
+	for i := 0; i < 10; i++ {
+		pickTile(tg, tiles, t)
+	}
+}
+
+func pickTile(tg *TileBag, tiles []Tile, t *testing.T) {
+	pickedTile := tg.pickTile()
+	t.Log(fmt.Sprintf("Picked: %s", string(pickedTile.letter)))
+	found := false
+	for _, tile := range tiles {
+		if pickedTile == tile {
+			found = true
+		}
+	}
+	assert.True(t, found)
+}
